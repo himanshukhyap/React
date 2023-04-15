b97de14093e252813359ecabc06f260b135b93d5
Himanshu Kashyap <himanshu_kashyap@outlook.com>
2023-04-15 18:41:19 +0530

    fgfdg

---
+++ React_Concept/src/Questions/Question.js
@@ -0,0 +1,34 @@
+// What is React?
+// React is a JavaScript library. It allows developers to build reusable UI components that can be easily combined to create complex UIs.
+
+// What are the key features of React?
+// Some of the key features of React include:
+
+// Virtual DOM: React uses a virtual DOM to improve performance and update the UI efficiently.
+// Component-based architecture: React allows developers to create reusable UI components that can be easily combined to create complex UIs.
+// One-way data binding: React uses one-way data binding to ensure that changes to the data flow in a predictable way.
+
+
+// What is JSX?
+// JSX is a syntax extension for JavaScript that allows developers to write HTML-like syntax in their JavaScript code. It is used to describe the structure and content of React components.
+
+// What is a React component?
+// A React component is a reusable piece of UI that can be rendered to the DOM. Components can be either functional or class-based.
+
+// What is state in React?
+// State is a data structure in React that holds information about the component's current state. When the state changes, React re-renders the component to reflect the new state.
+
+// What is props in React?
+// Props are short for "properties". They are used to pass data from one component to another in React. Props are read-only and cannot be modified by the component that receives them.
+
+// What is the difference between state and props?
+// State is managed within a component and can be changed by the component itself, while props are passed from a parent component to a child component and cannot be modified by the child component.
+
+// What is the difference between a functional component and a class component in React?
+// Functional components are simpler and easier to read and write than class components. They are stateless and can be used for simple UI components. Class components, on the other hand, can have state and lifecycle methods, making them more powerful for complex UI components.
+
+// What is the difference between React and React Native?
+// React is a JavaScript library for building user interfaces for web applications, while React Native is a framework for building native mobile applications using JavaScript.
+
+// What is Redux?
+// Redux is a popular state management library for React. It provides a predictable and centralized way of managing application state.

018763c492ee6943fb0f9f69308a6e64a79de660
Himanshu Kashyap <himanshu_kashyap@outlook.com>
2024-09-08 16:16:32 +0530

    fdsd

--- React_Concept_details/src/Form/Input.js
+++ React_Concept_details/src/Form/Input.js
@@ -2,22 +2,28 @@ import React, { useState } from 'react'
 import { Form } from 'react-bootstrap'
 
 export default function Input() {
-    const [text, settext] = useState("See your result")
+    // const [text, settext] = useState("See your result")
+    var formValue = {}
     const myChangeHandler = (event) => {
 
         console.log(event.target.value)
-        settext(event.target.value)
+        // settext(event.target.value)
+        formValue.text=event.target.value;
+        console.log(formValue)
+    
     }
+  
     return (
         <div>
             <Form className="mt-5 text-center">
                 <input onChange={myChangeHandler}
                     type="text"
+                    GetData={formValue}
                 />
             </Form>
             <br />
 
-            <h5>{text}</h5>
+            <h5>{formValue.text}</h5>
         </div>
     )
 }
